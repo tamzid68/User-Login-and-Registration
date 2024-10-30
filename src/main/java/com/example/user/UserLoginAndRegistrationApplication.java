fa1f318a656e684887ef5c662dafaae3aaa79f4b
ASM Tamzid <atkthegreat9999@gmail.com>
2024-10-30 22:54:03 +0600

    new project added

---
+++ src/main/java/com/example/user/UserLoginAndRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginAndRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginAndRegistrationApplication.class, args);
+	}
+
+}
